5096d7d394cb71b0ec63469ff603d929a10806f6
Tuấn Vương <vanhtuan0409@gmail.com>
2018-06-06 17:28:21 +0700

    add from ip

--- server/main.go
+++ server/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 )
 
+func getOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
 func main() {
+	machineIP := getOutboundIP().String()
+
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusOK)
-		rw.Write([]byte("Hello world"))
+		msg := fmt.Sprintf("Hello from %s", machineIP)
+		rw.Write([]byte(msg))
 	})
 	log.Println("Server is starting at port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
